c1ab55e46f40bc1e65166dfba7f9c592fe95a4ce
Tyler Moore <tylermoore@Tylers-iMac.frontierlocal.net>
2020-02-27 14:48:46 -0500

    chore: delete package lock.json

--- src/components/HomeInfos.vue
+++ src/components/HomeInfos.vue
@@ -3,48 +3,34 @@
     <div class="q-pa-md row items-start q-gutter-md">
       <div class="row justify-center items-center">
         <div class="col-md-5 q-ml-lg q-mr-lg">
-          <p
-            v-if="$q.platform.is.desktop"
-            class="text-h4"
-          >
-            Located in Statesboro, Soyumi Asian Kitchen has been a breath of fresh air in the food scene of the deep south.
+          <p class="text-h4">
+            Located in Statesboro, Soyumi Asian Kitchen has been a breath of
+            fresh air in the food scene of the deep south.
           </p>
           <p class="text-h5">
-            Soyumi Asian Kitchen opened in August 2017 in Statesboro, GA. Fueled by the food journeys of the proprietors, Soyumi was developed as a concept to broaden the horizons of it’s patrons as well as it’s team members. Coined from the phrase that’s exclaimed when something tasty hits the spot: “its so yummy!”, we strive to provide an experience that is unlike any other.
-          </p>
-          <p
-            v-if="$q.platform.is.mobile"
-            class="text-h4"
-          >
-            Located in Statesboro, Soyumi Asian Kitchen has been a breath of fresh air in the food scene of the deep south.
+            Soyumi Asian Kitchen opened in August 2017 in Statesboro, GA. Fueled
+            by the food journeys of the proprietors, Soyumi was developed as a
+            concept to broaden the horizons of it’s patrons as well as it’s team
+            members. Coined from the phrase that’s exclaimed when something
+            tasty hits the spot: “its so yummy!”, we strive to provide an
+            experience that is unlike any other.
           </p>
         </div>
         <div class="col-md-5 q-ml-lg q-mr-lg">
-          <p
-            v-if="$q.platform.is.desktop"
-            class="text-h5"
-          >
-            Phone <br>
-            (912) 871-5999 <br>
-            We deliver! Call us! <br>
-            <br>
-            Location <br>
-            727 Buckhead Drive <br>
-            Statesboro, GA 30458 <br>
-            <br>
-            Hours <br>
-            Sunday - Thursday <br>
-            11:00am-9:00pm <br>
-            Friday-Saturday <br>
+          <p class="text-h4">
+            Phone <br />
+            (912) 871-5999 <br />
+            We deliver! Call us! <br />
+            <br />
+            Location <br />
+            727 Buckhead Drive <br />
+            Statesboro, GA 30458 <br />
+            <br />
+            Hours <br />
+            Sunday - Thursday <br />
+            11:00am-9:00pm <br />
+            Friday-Saturday <br />
             11:00am-10:00p,
-          </p>
-          <p
-            v-if="$q.platform.is.mobile"
-            class="text-h4"
-          >
-            Ray The Caribbean Barber is a multicultural barber and Gentlemens
-            Hair Stylist. With amazing sicssors and the razor blade skils. Ray
-            has 15 years of exp in the Gentlemens Hair Style area.
           </p>
         </div>
       </div>
